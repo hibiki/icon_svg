08d94ebc47525675aad577c29441607e46f34a5a
hibiki <rosywish@gmail.com>
2016-06-22 23:44:36 +0900

    諸々調整、npm run build で動くように

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 ## icon SVG のテスト
 
+### 注意点
+
 - アイコン用のSVGファイルは、すべて高さを揃える
+
+### 出力
+
+`npm run build`
+
+分からなくなったらとりあえず `npm run` すればコマンドが見れる。
+
+### ファイル構造
+
+出力前
+```
+┗/v3/
+　┣/make_svg_sprite/
+　┃┣/svg/
+　┃┗_template.scss
+```
+
+出力後
+```
+┗/v3/
+　┣/make_svg_sprite/
+　┃┣/svg/
+　┃┗_template.scss
+　┣/css/
+　┃┗_svg_sprite.scss
+　┗/img/
+　　┗svg_sprite.svg
+```
+
+パスは後で調整するものとして仮
--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,23 @@ gulp.task( 'svgSprite', function () {
 
   var SVGSize = 128; // 個別の SVG アイコン作成時に統一した高さ。(今回は 128 px)
 
-  return gulp.src( [ './src/SVGSprite/*.svg' ] )
+  return gulp.src( [ './v3/make_svg_sprite/svg/*.svg' ] )
   .pipe( svgSpriteSheet( {
     // SCSSテンプレートの中で利用する値
-    cssPathSvg: '../SVGSprite.svg',
+    cssPathSvg: './v3/img/svg_sprite.svg',
     // ガター（スプライト同士の余白）
     padding: 10,
     pixelBase: SVGSize,
     // スプライトの配置設定
     positioning: 'packed',
     // 用意したSCSSテンプレート
-    templateSrc: './src/SVGSprite/_template.scss',
+    templateSrc: './v3/make_svg_sprite/_template.scss',
     // テンプレートから生成されるSCSSファイル
-    templateDest: './src/scss/_SVGSprite.scss',
+    templateDest: './v3/css/scss/_svg_sprite.scss',
     units: 'em'
   } ) )
-  .pipe( gulp.dest( './build/SVGSprite.svg' ) );
+  .pipe( gulp.dest( './v3/img/svg_sprite.svg' ) );
 
 } );
+
+// gulp.task('default', ['svgSprite']);
