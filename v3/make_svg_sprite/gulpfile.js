58941a4ef2bfc9f29b308784ad7f7ccacbeee7d0
hibiki <rosywish@gmail.com>
2016-06-23 00:36:40 +0900

    パス更新と書き出した後のものも一緒に入れる

--- v3/make_svg_sprite/gulpfile.js
+++ v3/make_svg_sprite/gulpfile.js
@@ -6,22 +6,22 @@ gulp.task( 'svgSprite', function () {
 
   var SVGSize = 128; // 個別の SVG アイコン作成時に統一した高さ。(今回は 128 px)
 
-  return gulp.src( [ './v3/make_svg_sprite/svg/*.svg' ] )
+  return gulp.src( [ '../img/svg/icon/*.svg' ] )
   .pipe( svgSpriteSheet( {
     // SCSSテンプレートの中で利用する値
-    cssPathSvg: './v3/img/svg_sprite.svg',
+    cssPathSvg: '/v3/img/svg/svg_sprite.svg',
     // ガター（スプライト同士の余白）
     padding: 10,
     pixelBase: SVGSize,
     // スプライトの配置設定
     positioning: 'packed',
     // 用意したSCSSテンプレート
-    templateSrc: './v3/make_svg_sprite/_template.scss',
+    templateSrc: '../make_svg_sprite/_template.scss',
     // テンプレートから生成されるSCSSファイル
-    templateDest: './v3/css/scss/_svg_sprite.scss',
+    templateDest: '../css/scss/_svg_sprite.scss',
     units: 'em'
   } ) )
-  .pipe( gulp.dest( './v3/img/svg_sprite.svg' ) );
+  .pipe( gulp.dest( '../img/svg/svg_sprite.svg' ) );
 
 } );
 
